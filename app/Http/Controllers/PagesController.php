ade8db05e35db9a3e68223d7d7044b2cdd50ae90
Mohd Khaizur bin. Khairuddin <khaizur@mampunet.btm>
2019-10-01 12:29:41 +0800

    initial commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        return view('pages.index');
+    }
+
+    public function service(){
+        return view('pages.service');
+    }
+
+    public function contact(){
+        return view('pages.contact');
+    }
+}
